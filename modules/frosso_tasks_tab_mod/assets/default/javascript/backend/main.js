8abf456afab4e2ae892508d71792b2e4493e4e8b
Francesco R <frosso@remedia.it>
2013-01-11 03:43:03 -0500

    TaskTabMod: fix javascript alla creazione di un task

--- modules/frosso_tasks_tab_mod/assets/default/javascript/backend/main.js
+++ modules/frosso_tasks_tab_mod/assets/default/javascript/backend/main.js
@@ -6,7 +6,7 @@
  */
 App.Wireframe.Utils['renderAttachmentsIndicator'] = function (has_attachments) {
 
-	if(typeof(has_attachments) != undefined && has_attachments != false){
+	if(typeof(has_attachments) != undefined && has_attachments == true){
 		return '<img src="' + App.Wireframe.Utils.imageUrl('16x16/icon-attachments.png', 'frosso_tasks_tab_mod') + '" title="' + App.lang("This item has attachments") + '" class="has_attachments"/>';
 	}
 	
