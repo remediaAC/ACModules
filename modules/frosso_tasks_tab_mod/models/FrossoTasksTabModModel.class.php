4d37ae5105cb1d07bb52225768e1d27070d5b4df
Francesco R <frosso@remedia.it>
2013-01-16 03:48:50 -0500

    TasksTabMod: fix with estimates

--- modules/frosso_tasks_tab_mod/models/FrossoTasksTabModModel.class.php
+++ modules/frosso_tasks_tab_mod/models/FrossoTasksTabModModel.class.php
@@ -27,10 +27,8 @@ class FrossoTasksTabModModel {
 						o.created_on,
 						o.updated_on,
 						o.due_on,
-						es.estimated_time,
 						rec.tracked_time
 					FROM " . TABLE_PREFIX . "project_objects o 
-					LEFT JOIN (SELECT parent_id, value AS estimated_time FROM " . TABLE_PREFIX . "estimates GROUP BY parent_id ORDER BY created_on DESC) es ON(o.id=es.parent_id)
 					LEFT JOIN (SELECT parent_id, sum(value) tracked_time FROM " . TABLE_PREFIX . "time_records WHERE state = ? GROUP BY(parent_id)) rec ON(o.id=rec.parent_id)
 					WHERE o.type = 'Task' AND o.project_id = ? AND o.state = ? AND o.visibility >= ?"
 				, $state, $project->getId(), $state, $user->getMinVisibility());
@@ -65,8 +63,8 @@ class FrossoTasksTabModModel {
 							'delegated_by_id'   => $task['delegated_by_id'],
 							'total_subtasks'    => $total_subtasks,
 							'open_subtasks'     => $open_subtasks,
-							'estimated_time'    => $taskObj->tracking()->canAdd($user) 	? $task['estimated_time'] 	: 0,
-							'tracked_time'      => $taskObj->tracking()->canAdd($user) 	? $task['tracked_time'] 	: 0,
+							'estimated_time'    => $taskObj->tracking()->canAdd($user) && $taskObj->tracking()->getEstimate() ? $taskObj->tracking()->getEstimate()->getValue() : 0,
+							'tracked_time'      => $taskObj->tracking()->canAdd($user) ? $task['tracked_time'] 	: 0,
 							'is_favorite'       => Favorites::isFavorite(array('Task', $task['id']), $user),
 							'is_archived'       => $task['state'] == STATE_ARCHIVED ? 1 : 0,
 							'visibility'        => $task['visibility'],
