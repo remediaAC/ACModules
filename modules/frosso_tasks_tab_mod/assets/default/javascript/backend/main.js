8e6ac9ad119b836bbead584b658508466439fb08
Francesco R <frosso89@gmail.com>
2013-02-21 21:48:38 +0100

    TaskTabMod: subscribers implementation

--- modules/frosso_tasks_tab_mod/assets/default/javascript/backend/main.js
+++ modules/frosso_tasks_tab_mod/assets/default/javascript/backend/main.js
@@ -18,12 +18,14 @@ App.Wireframe.Utils['renderAttachmentsIndicator'] = function (has_attachments) {
 App.Inspector.Properties.TaskSubscribers = function (object, client_interface) {
 	var subs_wrapper = $(this);
 	subs_wrapper.html("");
+	console.log(object);
 	var img = App.Wireframe.Utils.imageUrl('icons/12x12/edit.png', 'environment');
 	// subs_wrapper.append(img+' asd ');
 	if(object['subscribers'].length > 0) {
 		subs_wrapper.append(App.Wireframe.Utils.userLink(object['subscribers'][0]));
 		for (var i=1; i<object['subscribers'].length; i++) {
-			subs_wrapper.append(', ' + App.Wireframe.Utils.userLink(object['subscribers'][i]));
+			subs_wrapper.append(', ');
+			subs_wrapper.append(App.Wireframe.Utils.userLink(object['subscribers'][i]));
 		}
 	}
 };
