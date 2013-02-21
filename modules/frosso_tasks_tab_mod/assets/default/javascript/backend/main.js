1c54ffd4d2d2a33fcbe3d46be94d267bfdb110e4
Francesco R <frosso89@gmail.com>
2013-02-21 22:47:52 +0100

    TaskTabMod: subscription view & edit

--- modules/frosso_tasks_tab_mod/assets/default/javascript/backend/main.js
+++ modules/frosso_tasks_tab_mod/assets/default/javascript/backend/main.js
@@ -20,12 +20,25 @@ App.Inspector.Properties.TaskSubscribers = function (object, client_interface) {
 	subs_wrapper.html("");
 	console.log(object);
 	var img = App.Wireframe.Utils.imageUrl('icons/12x12/edit.png', 'environment');
-	// subs_wrapper.append(img+' asd ');
 	if(object['subscribers'].length > 0) {
 		subs_wrapper.append(App.Wireframe.Utils.userLink(object['subscribers'][0]));
 		for (var i=1; i<object['subscribers'].length; i++) {
 			subs_wrapper.append(', ');
 			subs_wrapper.append(App.Wireframe.Utils.userLink(object['subscribers'][i]));
 		}
+	}
+	if(object['options']['manage_subscriptions']) {
+		subs_wrapper.append(' ');
+		var subs_obj = object['options']['manage_subscriptions'];
+		var subs_img = "<img src='" + subs_obj["icon"] + "' alt='" + subs_obj['text'].clean() + "' />";
+		var subs_link = $('<a href="' + subs_obj.url.clean() + '" title="' + subs_obj["text"].clean() + '">' + subs_img + '</a>');
+		if (typeof (subs_obj["onclick"]) == "string" && subs_obj["onclick"]) {
+            var onclick;
+            eval("onclick = " + subs_obj["onclick"]);
+            if (typeof (onclick) == "function") {
+                onclick.apply(subs_link[0])
+            }
+        }
+        subs_wrapper.append(subs_link);
 	}
 };
