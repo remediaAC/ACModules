c740137b58e7d3c6a43092a087602aa3bb0361b0
Francesco R <frosso89@gmail.com>
2013-02-22 01:04:01 +0100

    TaskTabMod: version

--- modules/frosso_tasks_tab_mod/FrossoTasksTabModModule.class.php
+++ modules/frosso_tasks_tab_mod/FrossoTasksTabModModule.class.php
@@ -20,7 +20,7 @@ class FrossoTasksTabModModule extends ActiveCollabProjectSectionModule{
 	/*
 	 * Versione
 	 */
-	protected $version = '0.13';
+	protected $version = '0.13.2';
 	
 	/**
 	 * Name of the project object class (or classes) that this module uses
