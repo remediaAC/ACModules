e1a0c08f8b97e1fd1b87410de53380c204a7df48
Francesco R <frosso@remedia.it>
2013-02-26 04:37:47 -0500

    TaskTabMod: archive.tpl missing

--- modules/frosso_tasks_tab_mod/FrossoTasksTabModModule.class.php
+++ modules/frosso_tasks_tab_mod/FrossoTasksTabModModule.class.php
@@ -20,7 +20,7 @@ class FrossoTasksTabModModule extends ActiveCollabProjectSectionModule{
 	/*
 	 * Versione
 	 */
-	protected $version = '0.13.3';
+	protected $version = '0.13.4';
 	
 	/**
 	 * Name of the project object class (or classes) that this module uses
