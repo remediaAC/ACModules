d6e5bd828d53f69b0130a431b21680cd04a785fd
Francesco R <frosso89@gmail.com>
2013-03-15 00:09:11 +0100

    fu*, wrong repo

--- moduli/nome/nome.php
+++
@@ -1,7 +0,0 @@
-<?php
-//controlla che l'accesso sia effettuato dall'interno del programma
-defined( '_VALID_SESSION' ) or die( 'Restricted access' );
-
-echo $this->getAttributo('contenuto');
-
-?>
